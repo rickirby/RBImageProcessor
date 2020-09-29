fcf10ddee0f696e28b5ffbdd958b985e7628c265
Ricki Bin Yamin <ricki.bin.yamin16@mhs.ee.its.ac.id>
2020-09-29 08:33:38 +0700

    Initial Commit

---
+++ RBImageProcessor/RBImageProcessor.h
@@ -0,0 +1,19 @@
+//
+//  RBImageProcessor.h
+//  RBImageProcessor
+//
+//  Created by Ricki Private on 29/09/20.
+//  Copyright © 2020 Ricki Bin Yamin. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for RBImageProcessor.
+FOUNDATION_EXPORT double RBImageProcessorVersionNumber;
+
+//! Project version string for RBImageProcessor.
+FOUNDATION_EXPORT const unsigned char RBImageProcessorVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RBImageProcessor/PublicHeader.h>
+
+
