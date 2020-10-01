8247e85779c3242341db086fb8164f39d253b481
Ricki Bin Yamin <ricki.bin.yamin16@mhs.ee.its.ac.id>
2020-10-01 08:00:12 +0700

    done splitting

--- RBImageProcessor/ConvertColor.h
+++ RBImageProcessor/ConvertColor.h
@@ -12,13 +12,8 @@
 
 @interface ConvertColor : NSObject
 
-+ (UIImage *) imageBGRtoGRAY: (UIImage *) image;
-+ (UIImage *) imageBGRtoGRAYMagic: (UIImage *) image;
-+ (UIImage *) imageBGRtoBlackAndWhite: (UIImage *) image;
-+ (UIImage *) imageBGRtoNeg: (UIImage *) image;
-+ (UIImage *) imageBGRtoNegMagic: (UIImage *) image;
-+ (UIImage *) imageBGRtoHSV: (UIImage *) image;
-+ (UIImage *) imageBGRtoMagic: (UIImage *) image;
-+ (UIImage *) imageBGRtoAdaptive: (UIImage *) image;
+- (instancetype)initWithImage:(UIImage*)image;
+- (UIImage*)convertToGRAY;
+- (UIImage*)convertToHSV;
 
 @end
--- RBImageProcessor/ConvertColor.mm
+++ RBImageProcessor/ConvertColor.mm
@@ -10,155 +10,31 @@
 #import <opencv2/imgcodecs/ios.h>
 #import <Foundation/Foundation.h>
 #import "ConvertColor.h"
+#import "ConvertColorProcessor.hpp"
 
 using namespace std;
 using namespace cv;
 
-@implementation ConvertColor
-
-+ (UIImage *) imageBGRtoGRAY:(UIImage *)image {
-    // convert UIImage to Mat
-    cv::Mat openCVImage;
-    UIImageToMat(image, openCVImage, true);
-    
-    // Run the c++ code
-    cv::Mat resultImage = convertBGRtoGRAY(openCVImage);
-    
-    // convert Mat to UIImage and return
-    return MatToUIImage(resultImage);
+@implementation ConvertColor {
+	ConvertColorProcessor *_convertColorProcessor;
 }
 
-+ (UIImage *) imageBGRtoGRAYMagic:(UIImage *)image {
-    // convert UIImage to Mat
-    cv::Mat openCVImage;
-    UIImageToMat(image, openCVImage, true);
-    
-    // Run the c++ code
-    cv::Mat magicImage = convertBGRtoMagic(openCVImage);
-    cv::Mat resultImage = convertBGRtoGRAY(magicImage);
-    
-    // convert Mat to UIImage and return
-    return MatToUIImage(resultImage);
+- (instancetype)initWithImage:(UIImage*)image {
+	if ((self = [super init])) {
+		Mat opencvImage;
+		UIImageToMat(image, opencvImage);
+		_convertColorProcessor = new ConvertColorProcessor(opencvImage);
+	}
+	
+	return self;
 }
 
-+ (UIImage *) imageBGRtoBlackAndWhite:(UIImage *)image {
-    cv::Mat openCVImage;
-    UIImageToMat(image, openCVImage);
-    
-    cv::Mat resultImage = convertBGRtoBlackAndWhite(openCVImage);
-    
-    return MatToUIImage(resultImage);
+- (UIImage*)convertToGRAY {
+	return MatToUIImage(_convertColorProcessor->convertToGRAY());
 }
 
-+ (UIImage *) imageBGRtoNeg:(UIImage *)image {
-    // convert UIImage to Mat
-    cv::Mat openCVImage;
-    UIImageToMat(image, openCVImage);
-    
-    // Run the c++ code
-    cv::Mat resultImage = convertBGRtoNeg(openCVImage);
-    
-    // convert Mat to UIImage and return
-    return MatToUIImage(resultImage);
-}
-
-+ (UIImage *) imageBGRtoNegMagic:(UIImage *)image {
-    // convert UIImage to Mat
-    cv::Mat openCVImage;
-    UIImageToMat(image, openCVImage);
-    
-    // Run the c++ code
-    cv::Mat magicImage = convertBGRtoMagic(openCVImage);
-    cv::Mat resultImage = convertBGRtoNeg(magicImage);
-    
-    // convert Mat to UIImage and return
-    return MatToUIImage(resultImage);
-}
-
-+ (UIImage *) imageBGRtoHSV:(UIImage *)image {
-    // convert UIImage to Mat
-    cv:: Mat openCVImage;
-    UIImageToMat(image, openCVImage);
-    
-    // Run the c++ code
-    cv::Mat resultImage = convertBGRtoHSV(openCVImage);
-    
-    // convert Mat to UIImage and return
-    return MatToUIImage(resultImage);
-}
-
-+ (UIImage *) imageBGRtoMagic:(UIImage *)image {
-    // convert UIImage to Mat
-    cv::Mat openCVImage;
-    UIImageToMat(image, openCVImage);
-    
-    // Run the c++ code
-    cv::Mat resultImage = convertBGRtoMagic(openCVImage);
-    
-    // convert Mat to UIImage and return
-    return MatToUIImage(resultImage);
-}
-
-+ (UIImage *) imageBGRtoAdaptive:(UIImage *)image {
-    cv:: Mat openCVImage;
-    UIImageToMat(image, openCVImage);
-    
-    cv:: Mat resultImage = convertBGRtoAdaptive(openCVImage);
-    
-    return MatToUIImage(resultImage);
-}
-
-Mat convertBGRtoGRAY(Mat frame) {
-    Mat gray;
-    cvtColor(frame, gray, COLOR_BGR2GRAY);
-    
-    return gray;
-}
-
-Mat convertBGRtoBlackAndWhite(Mat frame) {
-    Mat gray = convertBGRtoGRAY(frame);
-    Mat bw;
-    threshold(gray, bw, 0, 255, THRESH_OTSU);
-    
-    return bw;
-}
-
-Mat convertBGRtoNeg(Mat frame) {
-    Mat gray;
-    Mat neg;
-    cvtColor(frame, gray, COLOR_BGR2GRAY);
-    neg.create(gray.rows, gray.cols, CV_8U);
-    // then invert the value
-    for (int j = 0; j < gray.rows; j++) {
-        for (int i = 0; i < gray.cols; i++) {
-            uchar intensity = gray.at<uchar>(j, i);
-            neg.at<uchar>(j, i) = 255 - intensity;
-        }
-    }
-    
-    return neg;
-}
-
-Mat convertBGRtoHSV(Mat frame) {
-    Mat hsv;
-    cvtColor(frame, hsv, COLOR_BGR2HSV);
-    return hsv;
-}
-
-Mat convertBGRtoMagic(Mat frame) {
-    Mat resultImage = cv::Mat::zeros(frame.size(), frame.type());
-    frame.convertTo(resultImage, -1, 1.9, -80);
-    frame.release();
-
-    return resultImage;
-}
-
-Mat convertBGRtoAdaptive(Mat frame) {
-    Mat grayImage = convertBGRtoGRAY(frame);
-    Mat resultImage = cv::Mat::zeros(grayImage.size(), grayImage.type());
-    adaptiveThreshold(grayImage, resultImage, 255, CV_ADAPTIVE_THRESH_GAUSSIAN_C, CV_THRESH_BINARY, 57, 10);
-    
-    return resultImage;
+- (UIImage*)convertToHSV {
+	return MatToUIImage(_convertColorProcessor->convertToHSV());
 }
 
 @end
