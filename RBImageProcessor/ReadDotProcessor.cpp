e0f37df91bfada676c91bc680b33d2d28a13f4f6
Ricki Bin Yamin <ricki.bin.yamin@gmail.com>
2020-12-07 21:53:41 +0700

    add read dot processor class

---
+++ RBImageProcessor/ReadDotProcessor.cpp
@@ -0,0 +1,9 @@
+//
+//  ReadDotProcessor.cpp
+//  RBImageProcessor
+//
+//  Created by Ricki Bin Yamin on 07/12/20.
+//  Copyright © 2020 Ricki Bin Yamin. All rights reserved.
+//
+
+#include "ReadDotProcessor.hpp"
---
+++ RBImageProcessor/ReadDotProcessor.hpp
@@ -0,0 +1,23 @@
+//
+//  ReadDotProcessor.hpp
+//  RBImageProcessor
+//
+//  Created by Ricki Bin Yamin on 07/12/20.
+//  Copyright © 2020 Ricki Bin Yamin. All rights reserved.
+//
+
+#ifndef ReadDotProcessor_hpp
+#define ReadDotProcessor_hpp
+
+#include <stdio.h>
+#include <opencv2/opencv.hpp>
+
+using namespace cv;
+
+class ReadDotProcessor {
+public:
+	
+	
+};
+
+#endif /* ReadDotProcessor_hpp */
