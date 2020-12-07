154e235a95b247cb22abe5c67dacbbe3afd18646
Ricki Bin Yamin <ricki.bin.yamin@gmail.com>
2020-12-07 22:21:31 +0700

    add method and properties declaration

--- RBImageProcessor/ReadDotProcessor.hpp
+++ RBImageProcessor/ReadDotProcessor.hpp
@@ -17,6 +17,23 @@ using namespace cv;
 class ReadDotProcessor {
 public:
 	
+	// MARK: - Life Cycles
+	
+	ReadDotProcessor(bool adaptiveType, int adaptiveBlockSize, double adaptiveConstant, int dilateIteration, int erodeIteration);
+	~ReadDotProcessor();
+	
+	// MARK: - Public Methods
+	
+	void blobAnalysis();
+	
+private:
+	
+	// MARK: - Private Properties
+	bool _adaptiveType;
+	int _adaptiveBlockSize;
+	double _adaptiveConstant;
+	int _dilateIteration;
+	int _erodeIteration;
 	
 };
 
