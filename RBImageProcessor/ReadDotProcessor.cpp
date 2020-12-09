194f16f0f5bf6687921e1d85fc125df906af120a
Ricki Bin Yamin <ricki.bin.yamin@gmail.com>
2020-12-09 19:06:57 +0700

    clone for new method

--- RBImageProcessor/ReadDotProcessor.cpp
+++ RBImageProcessor/ReadDotProcessor.cpp
@@ -72,6 +72,93 @@ Mat ReadDotProcessor::blobAnalysis(Mat image) {
 	return erodeImage;
 }
 
+Mat ReadDotProcessor::dotCircle(Mat image) {
+	
+	Mat gray, adaptiveImage, dilateImage, erodeImage, result;
+	vector<vector<Point>> contours;
+	
+	// Image Pre processing
+	
+	cvtColor(image, gray, COLOR_BGR2GRAY);
+	adaptiveThreshold(gray, adaptiveImage, 255, _adaptiveType ? ADAPTIVE_THRESH_GAUSSIAN_C : ADAPTIVE_THRESH_MEAN_C, THRESH_BINARY, _adaptiveBlockSize, _adaptiveConstant);
+	dilate(adaptiveImage, dilateImage, Mat(), Point(-1, -1), _dilateIteration);
+	erode(dilateImage, erodeImage, Mat(), Point(-1, -1), _erodeIteration);
+	
+	// Blob Analysis
+	
+	findContours(erodeImage, contours, noArray(), RETR_LIST, CHAIN_APPROX_SIMPLE);
+	
+	vector<vector<Point>> filteredContours;
+	
+	cout << "=== filtering contours" << endl;
+	
+	for (int i = 0; i < contours.size(); i++) {
+		double currentArea = contourArea(contours[i]);
+		if ((currentArea > 200.0) && (currentArea < 500.0)) {
+			filteredContours.push_back(contours[i]);
+		}
+	}
+	
+	cout << "=== eliminating " << contours.size() - filteredContours.size() << " contours" << endl;
+	
+	cout << "=== findContours debugging" << endl;
+	
+	for (unsigned int i = 0;  i < filteredContours.size();  i++) {
+		cout << "# Index: " << i << ", points count: " << filteredContours[i].size() << endl;
+		for (unsigned int j = 0;  j < filteredContours[i].size();  j++) {
+			cout << "Point(x,y)=" << filteredContours[i][j] << endl;
+		}
+		
+		cout << " Area: " << contourArea(filteredContours[i]) << endl;
+	}
+	
+	// Finding min max coordinate per dot and average them to get center point
+	
+	vector<Point> centerContoursPoint;
+	
+	for (unsigned int i = 0; i < filteredContours.size(); i++) {
+		
+		int minX = filteredContours[i][0].x, minY = filteredContours[i][0].y, maxX = filteredContours[i][0].x, maxY = filteredContours[i][0].y;
+		
+		for (unsigned int j = 0; j < filteredContours[i].size(); j++) {
+			
+			int currentX = filteredContours[i][j].x;
+			int currentY = filteredContours[i][j].y;
+			
+			minX = min(minX, currentX);
+			minY = min(minY, currentY);
+			maxX = max(maxX, currentX);
+			maxY = max(maxY, currentY);
+		}
+		
+		int centerX = (minX + maxX) / 2;
+		int centerY = (minY + maxY) / 2;
+		
+		Point2i centerPoint(centerX, centerY);
+		centerContoursPoint.push_back(centerPoint);
+	}
+	
+	cout << "=== centerPoint debugging" << endl;
+	
+	for (unsigned int i = 0;  i < filteredContours.size();  i++) {
+		cout << "# Index: " << i << ", points count: " << filteredContours[i].size() << endl;
+		for (unsigned int j = 0;  j < filteredContours[i].size();  j++) {
+			cout << "Point(x,y)=" << filteredContours[i][j] << endl;
+		}
+		
+		cout << " Center: " << centerContoursPoint[i] << endl;
+		cout << " Area: " << contourArea(filteredContours[i]) << endl;
+	}
+	
+	erodeImage = Scalar::all(0);
+	
+	for (unsigned int i = 0; i < centerContoursPoint.size(); i++) {
+		circle(erodeImage, centerContoursPoint[i], 10, Scalar::all(255), -1);
+	}
+	
+	return erodeImage;
+}
+
 Mat ReadDotProcessor::lineCoordinate(Mat image) {
 	
 	Mat gray, adaptiveImage, dilateImage, erodeImage, result;
