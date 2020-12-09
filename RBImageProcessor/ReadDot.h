68e09fa6e18927339c548d1c15d804474978afb6
Ricki Bin Yamin <ricki.bin.yamin@gmail.com>
2020-12-09 11:07:31 +0700

    add read dot file

---
+++ RBImageProcessor/ReadDot.h
@@ -0,0 +1,13 @@
+//
+//  ReadDot.h
+//  RBImageProcessor
+//
+//  Created by Ricki Bin Yamin on 08/12/20.
+//  Copyright © 2020 Ricki Bin Yamin. All rights reserved.
+//
+
+#ifndef ReadDot_h
+#define ReadDot_h
+
+
+#endif /* ReadDot_h */
---
+++ RBImageProcessor/ReadDot.mm
@@ -0,0 +1,9 @@
+//
+//  ReadDot.m
+//  RBImageProcessor
+//
+//  Created by Ricki Bin Yamin on 08/12/20.
+//  Copyright © 2020 Ricki Bin Yamin. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
